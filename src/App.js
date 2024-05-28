6a5a20b51e2fd2a21b4e3233cd30d7efeb3ddce8
lsljw65 <48504421+lsljw65@users.noreply.github.com>
2024-05-28 20:40:49 +0900

    HOOK, put, post

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import CreateDay from "./component/CreateDay";
+import CreateWords from "./component/CreateWord";
 import Day from "./component/Day";
 import DayList from "./component/DayList";
 import EmptyPage from "./component/EmptPage";
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList/>} />
           <Route path="/day/:day" element={<Day/>}/>
+          <Route path="/create_word" element={<CreateWords/>}/>
+          <Route path="/create_day" element={<CreateDay/>}/>
           <Route path="/*" element={<EmptyPage/>}/>
         </Routes>
       </div>
--- src/component/Day.js
+++ src/component/Day.js
@@ -1,20 +1,35 @@
-import dummy from "../db/data.json";
+// import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
+// import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function Day(){
     const {day}=useParams();
-    const wordList=dummy.words.filter(word=>(
-        word.day===Number(day)
-    ))
+    // const wordList=dummy.words.filter(word=>(
+    //     word.day===Number(day)
+    // ))
+    // const [words,setWords]=useState([])
 
-    console.log(day)
+    // useEffect(()=>{
+    //     fetch(`http://localhost:3001/words?day=${day}`)
+    //     .then(res=>{
+    //         return res.json();
+    //     })
+    //     .then(data=>{
+    //         setWords(data);
+    //     })
+    // },[day])
+    // console.log(day)
+
+    const words=useFetch(`http://localhost:3001/words?day=${day}`)
+
     return(
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word=>(
+                    {words.map(word=>(
                         <Word word={word} key={word.id}/>
                     ))}
                     
